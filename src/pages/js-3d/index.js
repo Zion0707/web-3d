eebd05c7cdc30e033a9d1ef93b92714a5b17f2c7
huangzihao <346445684@qq.com>
2021-03-04 14:14:33 +0800

    change

--- src/pages/js-3d/index.js
+++ src/pages/js-3d/index.js
@@ -1,10 +1,5 @@
 import React, { useEffect, useState, Fragment } from 'react';
 import { message } from 'antd';
-import * as THREE from 'three';
-import TWEEN from '@tweenjs/tween.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-
 import Script from 'react-load-script';
 import { useHistory } from 'react-router-dom';
 
@@ -15,8 +10,13 @@ const Js3d = () => {
     const [url, setUrl] = useState('');
     const history = useHistory();
 
-    // 外部cdn调用函数
-    window.ddddd = (name) => {
+    // 外部cdn调用函数（加载）
+    window.model3dLoad = () => {
+        console.log('3d 模型已加载');
+    };
+
+    // 外部cdn调用函数（事件）
+    window.model3dEvent = (name) => {
         // console.log(name);
         message.warning(`点击了${name}`);
     };
@@ -29,21 +29,12 @@ const Js3d = () => {
     };
     const scriptOnLoad = () => {
         console.log('onLoad');
-        // js 加载完毕的时候执行初始化
-        if (window.js3dModel) {
-            window.js3dModel.init({
-                THREE,
-                TWEEN,
-                OrbitControls,
-                OBJLoader,
-            });
-        }
     };
 
     const init = () => {
         // 模拟ajax
         const timer = setTimeout(() => {
-            setUrl('/cdn/model/js-3d/index.min.js?v=' + Date.parse(new Date()));
+            setUrl('/cdn/model/item1/src/index.min.js?v=' + Date.parse(new Date()));
             clearTimeout(timer);
         }, 300);
     };
